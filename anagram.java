ea81aa3afe9b12bb515a67b6120d770910df9101
Monojit Pal <144504389+Monojit-Pal@users.noreply.github.com>
2025-04-13 19:22:53 +0530

    Create anagram.java

---
+++ anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] freq = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            freq[s2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

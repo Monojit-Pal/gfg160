ba0f9a427c5af9e7238e3d4d04f6f4f074bdf2e6
Monojit Pal <144504389+Monojit-Pal@users.noreply.github.com>
2025-04-30 18:09:55 +0530

    Create aggressiveCows.java

---
+++ aggressiveCows.java
@@ -0,0 +1,32 @@
+class Solution {
+    public static int aggressiveCows(int[] stalls, int k) {
+        Arrays.sort(stalls);
+        int low = 1;
+        int high = stalls[stalls.length - 1] - stalls[0];
+        int result = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canPlaceCows(stalls, k, mid)) {
+                result = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+    
+    private static boolean canPlaceCows(int[] stalls, int k, int distance) {
+        int count = 1;
+        int lastPosition = stalls[0];
+        for (int i = 1; i < stalls.length; i++) {
+            if (stalls[i] - lastPosition >= distance) {
+                count++;
+                lastPosition = stalls[i];
+            }
+            if (count >= k)
+                return true;
+        }
+        return false;
+    }
+}
